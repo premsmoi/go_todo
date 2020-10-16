11222bb89ca199ed075e6e537f059ae4feb3cd57
Generalkhun <khun2551@hotmail.com>
2020-10-16 14:08:32 +0700

    Added routergroup for router in main.go

--- server/main.go
+++ server/main.go
@@ -7,15 +7,20 @@ func main() {
 	router := gin.Default()
 	//Login-router (coming soon)
 
-	router.Group("/test")
+	//router.Group("/test")
 	//routerLogin := router.GroupRouterLongin()
 
 	//task-router
-	// routerTask := router.Group("/task"){
-	// 	task.POST()
-	// }
+	routerTask := router.Group("/v1")
+	{
+		routerTask.GET("/api/task", middleware.GetAllTask)
+		routerTask.POST("/api/task", middleware.CreateTask)
+		routerTask.PUT("/api/undoTask/", middleware.UndoTask)
+		routerTask.DELETE("/api/deleteTask", middleware.DeleteTask)
+		routerTask.DELETE("/api/deleteAllTask", middleware.DeleteAllTask)
+	}
 
-	//task
+	//Run the server
 	router.Run(":8080")
 
 }
