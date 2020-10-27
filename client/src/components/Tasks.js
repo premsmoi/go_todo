d60ea3c05a20e40164c27eccdc90bae5929cc2a9
Generalkhun <khun2551@hotmail.com>
2020-10-27 16:18:39 +0700

    Add component to Tasks page

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -1,96 +1,113 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 import axios from "axios";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Cookies from 'js-cookie';
-import {
-  Button,
-  Form,
-  Grid,
-  Header,
-  Image,
-  Message,
-  Segment,
-} from "semantic-ui-react";
-let endpoint = "http://localhost:8080";
+import { Card, Header, Form, Input, Icon, Button, ItemDescription } from "semantic-ui-react";
 
-function Tasks() {
-  // set state variables
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [login, setLogin] = useState(false);
-  // Functions
-  function onChangeHandlerUsername(event) {
-    setUsername(event.target.value);
-    console.log(username);
+let endpoint = "http://127.0.0.1:8080";
+
+function Tasks(props) {
+  // state variable
+  const [task, setTask] = useState("");
+  const [item,setItem] = useState([])
+
+  //Handler functions
+  function onChangeHandlers(event) {
+    setTask(event.target.value);
   }
-
-  function onChangeHandlerPassword(event) {
-    setPassword(event.target.value);
-    console.log(password);
+  function onSubmitHandler() {
+    console.log("pRINTING task", task);
+    if (task) {
+      axios
+        .post(
+          endpoint + "/task",
+          {
+            task,
+          },
+          {
+            withCredentials: true,
+          }
+        )
+        .then((res) => {
+          getTask();
+          setTask("");
+          console.log(res);
+        });
+    }
   }
+  //task function
+  // gettask: get all task from db
+  function getTask() {
+    axios.get(endpoint + "/task").then((res) => {
+      console.log(res);
+      if (res.data) {
+        setItem(
+          res.data.map((item) => {
+            let color = "yellow";
+            if (item.status) {
+              color = "green";
+            }
+            return (
+              <Card key={item._id} color={color} fluid>
+                <Card.Content>
+                  <Card.Header textAlign="left">
+                    <div style={{ wordWrap: "break-word" }}>{item.task}</div>
+                  </Card.Header>
 
-  function submitLogin(event) {
-    console.log("Submit login");
-    axios
-      .post(endpoint + "/auth/signin", {
-        username: username,
-        password: password,
-      })
-      .then(
-        function (response) {
-          setLogin(true);
-          console.log(response);
-          console.log(
-            "Successfully login, look at the cookie, you'll see the sent token"
-          );
-          console.log(Cookies.set("token"))
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
+                  <Card.Meta textAlign="right">
+                    <Icon
+                      name="check circle"
+                      color="green"
+                      onClick={() => this.updateTask(item._id)}
+                    />
+                    <span style={{ paddingRight: 10 }}>Done</span>
+                    <Icon
+                      name="undo"
+                      color="yellow"
+                      onClick={() => this.undoTask(item._id)}
+                    />
+                    <span style={{ paddingRight: 10 }}>Undo</span>
+                    <Icon
+                      name="delete"
+                      color="red"
+                      onClick={() => this.deleteTask(item._id)}
+                    />
+                    <span style={{ paddingRight: 10 }}>Delete</span>
+                  </Card.Meta>
+                </Card.Content>
+              </Card>
+            );
+            
+          })
+        )
+        
+      } else {
+        setItem([])
+      }
+    });
   }
-
   return (
-    <Router>
-      <Grid
-        textAlign="center"
-        style={{ height: "100vh" }}
-        verticalAlign="middle"
-      >
-        <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as="h2" color="teal" textAlign="center">
-            <Image src="/logo.png" /> Register your account
-          </Header>
-          <Form size="large">
-            <Segment stacked>
-              <Form.Input
-                fluid
-                icon="user"
-                iconPosition="left"
-                placeholder="E-mail address"
-                onChange={onChangeHandlerUsername}
-              />
-              <Form.Input
-                fluid
-                icon="lock"
-                iconPosition="left"
-                placeholder="Password"
-                type="password"
-                onChange={onChangeHandlerPassword}
-              />
-
-              <Button color="teal" fluid size="large" onClick={submitLogin}>
-                Login
-              </Button>
-            </Segment>
-          </Form>
-          <Message>
-            Not have account? <Link to="/register">Register</Link>
-          </Message>
-        </Grid.Column>
-      </Grid>
-    </Router>
+    <div>
+      <div className="row">
+        <Header className="header" as="h2">
+          Your todolist
+        </Header>
+      </div>
+      <div className="row">
+        <Form>
+          <Input
+            type="text"
+            name="task"
+            onChange={onChangeHandlers}
+            value={task}
+            fluid
+            placeholder="Create Task"
+          />
+          <Button onClick={onSubmitHandler}>Create Task</Button>
+        </Form>
+      </div>
+      <div className="row">
+        <Card.Group>{item}</Card.Group>
+      </div>
+    </div>
   );
 }
 
